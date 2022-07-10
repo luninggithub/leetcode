6f01d55a172f6022e4769fda16914ccf34d5d229
Lu <luning03@baidu.com>
2022-07-10 18:52:13 +0800

    Finish Offer Day28

--- app/src/test/java/com/baidu/duershow/leetcode/offer/Day28.java
+++ app/src/test/java/com/baidu/duershow/leetcode/offer/Day28.java
@@ -1,9 +1,20 @@
 package com.baidu.duershow.leetcode.offer;
 
+import org.junit.Test;
+
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Day28 {
+
+    @Test
+    public void addition_isCorrect() {
+        permutation("abc");
+    }
+
+
     /**
      * https://leetcode.cn/problems/xu-lie-hua-er-cha-shu-lcof/
      * 剑指 Offer 37. 序列化二叉树
@@ -71,8 +82,30 @@ public class Day28 {
      * @param s
      * @return
      */
+    List<String> res = new LinkedList<>();
+    char[] c;
     public String[] permutation(String s) {
-
+        c = s.toCharArray();
+        dfs(0);
+        return res.toArray(new String[res.size()]);
     }
-
+    void dfs(int x) {
+        if(x == c.length - 1) {
+            res.add(String.valueOf(c)); // 添加排列方案
+            return;
+        }
+        HashSet<Character> set = new HashSet<>();
+        for(int i = x; i < c.length; i++) {
+            if(set.contains(c[i])) continue; // 重复，因此剪枝
+            set.add(c[i]);
+            swap(i, x); // 交换，将 c[i] 固定在第 x 位
+            dfs(x + 1); // 开启固定第 x + 1 位字符
+            swap(i, x); // 恢复交换
+        }
+    }
+    void swap(int a, int b) {
+        char tmp = c[a];
+        c[a] = c[b];
+        c[b] = tmp;
+    }
 }
